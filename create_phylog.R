56f5ac6cd8d72fe70957e2e115f42b20f4c0f643
ricardosegovia <segoviacortes@gmail.com>
2023-05-29 15:51:48 -0400

    version

--- create_phylog.R
+++ create_phylog.R
@@ -1,7 +1,8 @@
-##creating an exploratory phylogeny
-#devtools::install_github("jinyizju/V.PhyloMaker")
+##Tutorial para crear una filogenia de un listado de especies
 
+#devtools::install_github("jinyizju/V.PhyloMaker")
 library(V.PhyloMaker)
+
 library(vegan)
 library(ape)
 library(phytools)
@@ -10,35 +11,46 @@ library(taxize)
 library(rgbif)
 
 
-andes<-read.csv("/home/ricardo/Documents/Colaboraciones/johnny_riffo/tesis/jonispps.csv")
-colnames(andes)
+# Abrimos un listado de especies para hacer un arbol filogenetico
+andes<-read.csv("jonispps.csv")
 head(andes)
 dim(andes)
 
-## creating the file requiered by V.phylomaker "SPECIES","GENUS","FAMILY"
+## V.phylomaker necesita un data frame con tres columnas llasdas: "species", "genus", "family", "species.relative", "genus.relative" 
+
+#Necesitamos separar el genero y conocer la familia.
+#En este caso usamos la funcion de correccion taxonomica de rgbif
 andes_temp <- name_backbone_checklist(andes$unique.spps.) #taxa biodata_corrected
 head(as.data.frame(andes_temp))
+#Es posible que algunos de estos nombres no sean aceptados
 table(andes_temp$status)
+#Seleccionaremos solo aquellos que son nombres aceptados
 andes_temp2 <- andes_temp[andes_temp$status == "ACCEPTED",]
 table(andes_temp2$status)
 head(as.data.frame(andes_temp2))
 
-data <- as.data.frame(andes_temp2[c("species", "genus", "family")])
-
-species_list <- data
+#seleccionamos solo aquellas columnas que necesitamos
+species_list <- as.data.frame(andes_temp2[,c("species", "genus", "family")])
 head(species_list)
+
+#creamos las columnas de relatives que necesita V.Phylomaker
 species_list[, "species.relative"] <- NA
 species_list[, "genus.relative"] <- NA
-
+dim(species_list)
 write.csv(species_list, "species.csv")
 
-## Creating tree
-
+## Creamos el arbol
 tree<-phylo.maker(sp.list =species_list, tree = GBOTB.extended, nodes = nodes.info.1, scenarios="S3")
+#comprbamos el tipo de archivo creado
 str(tree)
+class(tree)
+str(tree$scenario.3)
+class(tree$scenario.3)
+
+#guardamos el arbol
 write.tree(tree$scenario.3, "temp_tree.tre")
 
-## editing tree - matching names with the species file
+## Ahora editamos el arbol
 tree<-read.tree("temp_tree.tre")
 str(tree)
 newnames<-tree$tip.label
@@ -46,10 +58,14 @@ newnames <- gsub('_', ' ', newnames)
 
 tree$tip.label<-newnames
 
-write.tree(tree,"tree.tre")
-
-
+#el arbol tiene 343 especies
+dim(species_list)
 length(tree$tip.label)
+# hubo 15 especies que no fueron ubicadas
+cuales <- species_list[!species_list$species%in%tree$tip.label,]
+cuales
+
+write.tree(tree,"tree.tre")
 
 ## plotting tree
 plot(tree,no.margin=TRUE,edge.width=2)
@@ -71,12 +87,10 @@ tree_corto <-drop.tip(tree,species2remov)
 plotTree(tree_corto,type="fan",fsize=0.7,lwd=1,
     ftype="i")  
 
-plotTree(tree_corto,type="fan",fsize=0.7,lwd=1,
-    ftype="i")      
-
-
+#ahora vamos a escoger solo a las especies de Liliopsida
 node<-fastMRCA(tree,"Alstroemeria andina",
     "Mastigostyla cyrtophylla")
+node
 class(node)
 liliop<-extract.clade(tree,node)
 plotTree(liliop,ftype="i", node.numbers=T)
